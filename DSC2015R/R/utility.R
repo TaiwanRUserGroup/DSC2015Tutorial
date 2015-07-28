83d22535f0225aa8b3936b37ec89287b8308bf51
Wush Wu <wush978@gmail.com>
2015-07-29 00:13:33 +0800

    add install_course, slide

---
+++ DSC2015R/R/utility.R
@@ -0,0 +1,26 @@
+pkg_file <- function(...) {
+  system.file(..., package = .packageName)
+}
+
+install_course <- function() {
+  library(swirl)
+  if (!file.exists(get_swirl_option("courses_dir"))) {
+    dir.create(get_swirl_option("courses_dir"), recursive = TRUE)
+  }
+  cat("Installing swirl course for R basic...\n")
+  install_course_directory(pkg_file("basic/swirl/RBasic"))
+  cat("Installing swirl course for R ETL\n")
+  install_course_directory(pkg_file("etl/swirl"))
+  cat("Installing swirl course for R Visualization\n")
+  install_course_directory(pkg_file("visualization/swirl"))
+}
+
+slide <- function() {
+  cat("1: R swirl (DBoy) 2: R Basic (Ning) 3: R Basic (Noah)\n4: R ETL (aha) 5: Visualization (Kyle, Mansun, Ben)\n")
+  switch(readline(),
+         "1" = browseURL(pkg_file("basic/swirl/index.html")),
+         "2" = browseURL(pkg_file("basic/ForBasicSharing.html")),
+         "3" = stop("TODO"),
+         "4" = browseURL(pkg_file("etl/slide/index.html")),
+         "5" = browseURL(pkg_file("visualization/index.html")))
+}
