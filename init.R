23f58eec8d46e743ba8a35aa03d14741f38728d5
Wush Wu <wush978@gmail.com>
2015-08-18 17:33:10 +0800

    install pkg if the version is not the same

--- init.R
+++ init.R
@@ -4,6 +4,6 @@ local({
   pkgs.remote <- available.packages()
   pkgs.local <- installed.packages()
   pkgs <- intersect(rownames(pkgs.remote), rownames(pkgs.local))
-  pkgs.outdated <- package_version(pkgs.local[pkgs, "Version"]) < package_version(pkgs.remote[pkgs, "Version"])
+  pkgs.outdated <- package_version(pkgs.local[pkgs, "Version"]) != package_version(pkgs.remote[pkgs, "Version"])
   if (sum(pkgs.outdated) > 0) install.packages(names(which(pkgs.outdated)))
 })
